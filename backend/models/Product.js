eeeb9690ba6698756e6f8fb856ccd44d08e45c78
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2024-09-21 22:15:30 +0100

    Create Product.js

---
+++ backend/models/Product.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, enum: ['Skincare', 'Haircare', 'Supplements'], required: true },
+    image: { type: String },
+    inStock: { type: Boolean, default: true },
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
