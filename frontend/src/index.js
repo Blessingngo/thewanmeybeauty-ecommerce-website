baeefdf006391f0c06126857b54d77092e75ac13
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2024-09-21 22:26:22 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './store';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
 );
