726d15fcf147e5d544bc0b8fc8dd35a916b7e146
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2024-09-21 22:47:30 +0100

    Create Header.js

---
+++ frontend/src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/products">Products</Link>
+                <Link to="/cart">Cart</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
