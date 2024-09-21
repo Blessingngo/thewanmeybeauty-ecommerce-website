9e3735defd0cf95142bc84c85b50c5bd84044657
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2024-09-21 22:53:01 +0100

    Create OrderPage.js

---
+++ frontend/src/pages/OrderPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import OrderHistory from '../components/Orders/OrderHistory';
+
+const OrderPage = () => {
+    return (
+        <div>
+            <h1>Your Order History</h1>
+            <OrderHistory />
+        </div>
+    );
+};
+
+export default OrderPage;
